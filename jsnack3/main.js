f2463d15f573308f3023eea48f50e8eb4c0ec66c
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-04-12 14:51:37 +0200

    add comment in jsnack3 js code

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -2,11 +2,15 @@
 
 Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC) */
 
+
+//dichiaro una variabile in cui inserisco una parola
 let myString = 'ciao'
 
+//imposto una funzione che separi le lettere della stringa in un array con metodo split, inverta l'ordine di queste lettere con il metodo reverse e le aggiunga nel nuovo ordine con il metodo join
 function reverseString(myString){
  return myString.split("").reverse().join("");
 }
 
+//stampo in console il risultato della mia funzione
 console.log(reverseString(myString));
 
